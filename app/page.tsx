3e5231d3f65a28800dc419eeb8b42c1e8974caf8
Siditaher <omarsiditaher@gmail.com>
2025-11-29 18:11:55 +0100

    Fix Supabase type error and update project

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,209 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
+import { useEffect, useState, FormEvent } from "react";
+import { supabase } from "@/lib/supabase"
+import toast, { Toaster } from "react-hot-toast";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css"
+
+interface Student {
+  id?: string;
+  name: string;
+  email: string;
+  phone_number: string;
+  gender: string;
+}
+
 
 export default function Home() {
+
+  const [students, setStudents] = useState<Student[]>([])
+  const [form, setForm] = useState<Student>({
+    name: "",
+    email: "",
+    phone_number: "",
+    gender: "Male"
+  })
+
+  const [editId, setEditId] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetchStudents()
+  }, [])
+  //Handle Form Submit
+
+  async function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log(form)
+
+    if (editId) {
+      // Update
+      const { error } = await supabase.from("students").update([form]).eq("id", editId)
+
+      if (error) {
+        toast.error("Failed to update")
+      } else {
+        toast.success("Student updated successfully!")
+
+      }
+    } else {
+      //Add
+      const { error } = await supabase.from("students").insert([form])
+
+      if (error) {
+        toast.error(`Failed to create ${error.message}`)
+      } else {
+        toast.success("Student added successfully")
+      }
+
+      setForm({
+        name: "",
+        email: "",
+        phone_number: "",
+        gender: "Male"
+      })
+    }
+
+    fetchStudents()
+
+  }
+
+  // Fetch Students Data
+  async function fetchStudents() {
+    const { data, error } = await supabase.from("students").select("*")
+
+    if (error) {
+      toast.error(`Failed to create ${error.message}`)
+    } else {
+      console.log(data)
+      setStudents(data || [])
+    }
+  }
+  // Handel student Edit
+  function handStudentEdit(student: Student) {
+    setForm(student)
+    if (student.id) {
+      setEditId(student.id)
+    }
+  }
+  // Handel Student Delete
+  async function handStudentDelete(id: string) {
+
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    })
+
+    if (result.isConfirmed) {
+      const { error } = await supabase.from("students").delete().eq("id", id)
+
+      if (error) {
+        toast.error("Failed to delete student")
+      } else {
+        toast.success("Student deleted sucessfully")
+        fetchStudents()
+      }
+
+    }
+  }
+
+
+
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="container my-5">
+        <Toaster />
+        <h3 className="mb-4">Student Management</h3>
+        <div className="row">
+          { /** Left Side Form */}
+          <div className="col-md-4">
+            <div className="card mb-4">
+              <div className="card-body">
+                <form onSubmit={handleFormSubmit}>
+                  <div className="mb-3">
+                    <label className="from-label">Name</label>
+                    <input type="text" value={form.name} onChange={(event) => setForm({
+                      ...form,
+                      name: event.target.value
+
+                    })}
+                      className="form-control" />
+                  </div>
+                  <div className="mb-3">
+                    <label className="from-label">Email</label>
+                    <input type="email" value={form.email} onChange={(event) => setForm({
+                      ...form,
+                      email: event.target.value
+                    })} className="form-control" />
+                  </div>
+                  <div className="mb-3">
+                    <label className="from-label">Phone Number</label>
+                    <input type="text" value={form.phone_number} onChange={(event) => setForm({
+                      ...form,
+                      phone_number: event.target.value
+                    })} className="form-control" />
+                  </div>
+                  <div className="mb-3">
+                    <label className="from-label">Gender</label>
+                    <select className="form-select" value={form.gender} onChange={(event) => setForm({
+                      ...form,
+                      gender: event.target.value
+                    })}>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </select>
+                  </div>
+
+                  <button className="btn btn-primary w-100">Add</button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          { /**Right Side Form */}
+
+          <div className="col-md-8">
+            <div className="table-responsive">
+              <table className="table table-bordered">
+                <thead className="table-light">
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Gender</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    students.map((singleStudent) => (
+                      <tr key={singleStudent.id}>
+                        <td>{singleStudent.name}</td>
+                        <td>{singleStudent.email}</td>
+                        <td>{singleStudent.phone_number}</td>
+                        <td>{singleStudent.gender}</td>
+                        <td>
+                          <button className="btn btn-warning btn-sm me-2" onClick={() => handStudentEdit
+                            (singleStudent)}>
+                            Edit
+                          </button>
+                          <button className="btn btn-danger btn-sm me-2" onClick={() => singleStudent.id && handStudentDelete(singleStudent.id)}>Delete</button>
+                        </td>
+                      </tr>))
+                  }
+
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
